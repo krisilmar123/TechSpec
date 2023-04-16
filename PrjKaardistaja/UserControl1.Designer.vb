d4d5579302b48d8a7fbb7f059a6a344c2ffacefe
Smuutihh <92427508+Smuutihh@users.noreply.github.com>
2023-04-16 16:10:37 +0300

    Lisatud on Kaart Tallinna vaatega

--- PrjKaardistaja/UserControl1.Designer.vb
+++ PrjKaardistaja/UserControl1.Designer.vb
@@ -22,9 +22,45 @@ Partial Class UserControl1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.GMapControl1 = New GMap.NET.WindowsForms.GMapControl()
+        Me.SuspendLayout
+        '
+        'GMapControl1
+        '
+        Me.GMapControl1.Bearing = 0!
+        Me.GMapControl1.CanDragMap = true
+        Me.GMapControl1.EmptyTileColor = System.Drawing.Color.Navy
+        Me.GMapControl1.GrayScaleMode = false
+        Me.GMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow
+        Me.GMapControl1.LevelsKeepInMemory = 5
+        Me.GMapControl1.Location = New System.Drawing.Point(0, 0)
+        Me.GMapControl1.MarkersEnabled = true
+        Me.GMapControl1.MaxZoom = 2
+        Me.GMapControl1.MinZoom = 2
+        Me.GMapControl1.MouseWheelZoomEnabled = true
+        Me.GMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter
+        Me.GMapControl1.Name = "GMapControl1"
+        Me.GMapControl1.NegativeMode = false
+        Me.GMapControl1.PolygonsEnabled = true
+        Me.GMapControl1.RetryLoadTile = 0
+        Me.GMapControl1.RoutesEnabled = true
+        Me.GMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.[Integer]
+        Me.GMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(CType(CType(33,Byte),Integer), CType(CType(65,Byte),Integer), CType(CType(105,Byte),Integer), CType(CType(225,Byte),Integer))
+        Me.GMapControl1.ShowTileGridLines = false
+        Me.GMapControl1.Size = New System.Drawing.Size(797, 447)
+        Me.GMapControl1.TabIndex = 0
+        Me.GMapControl1.Zoom = 0R
+        '
+        'UserControl1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-    End Sub
+        Me.Controls.Add(Me.GMapControl1)
+        Me.Name = "UserControl1"
+        Me.Size = New System.Drawing.Size(800, 450)
+        Me.ResumeLayout(false)
 
+End Sub
+
+    Friend WithEvents GMapControl1 As GMap.NET.WindowsForms.GMapControl
 End Class
--- PrjKaardistaja/UserControl1.vb
+++ PrjKaardistaja/UserControl1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class UserControl1
-
+    Private Sub GMapControl1_Load(sender As Object, e As EventArgs) Handles GMapControl1.Load
+        GMapControl1.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance
+        GMapControl1.Position = New GMap.NET.PointLatLng(59.4370, 24.7536)
+        GMapControl1.MinZoom = 1
+        GMapControl1.MaxZoom = 18
+        GMapControl1.Zoom = 13
+        GMapControl1.Manager.Mode = GMap.NET.AccessMode.ServerAndCache
+        GMapControl1.CanDragMap = true
+        GMapControl1.DragButton = MouseButtons.Left
+    End Sub
 End Class
