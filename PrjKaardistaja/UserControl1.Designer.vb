e6466d291ba3e5ca2eb940ce0b478f7d5a9e8407
K2uritMain <kalili@ttu.ee>
2023-04-16 16:32:15 +0300

    Katsetus Liinidega

--- PrjKaardistaja/UserControl1.Designer.vb
+++ PrjKaardistaja/UserControl1.Designer.vb
@@ -34,30 +34,32 @@ Partial Class UserControl1
         Me.GMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow
         Me.GMapControl1.LevelsKeepInMemory = 5
         Me.GMapControl1.Location = New System.Drawing.Point(0, 0)
-        Me.GMapControl1.MarkersEnabled = true
+        Me.GMapControl1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GMapControl1.MarkersEnabled = True
         Me.GMapControl1.MaxZoom = 2
         Me.GMapControl1.MinZoom = 2
-        Me.GMapControl1.MouseWheelZoomEnabled = true
+        Me.GMapControl1.MouseWheelZoomEnabled = True
         Me.GMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter
         Me.GMapControl1.Name = "GMapControl1"
-        Me.GMapControl1.NegativeMode = false
-        Me.GMapControl1.PolygonsEnabled = true
+        Me.GMapControl1.NegativeMode = False
+        Me.GMapControl1.PolygonsEnabled = True
         Me.GMapControl1.RetryLoadTile = 0
-        Me.GMapControl1.RoutesEnabled = true
+        Me.GMapControl1.RoutesEnabled = True
         Me.GMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.[Integer]
-        Me.GMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(CType(CType(33,Byte),Integer), CType(CType(65,Byte),Integer), CType(CType(105,Byte),Integer), CType(CType(225,Byte),Integer))
-        Me.GMapControl1.ShowTileGridLines = false
-        Me.GMapControl1.Size = New System.Drawing.Size(797, 447)
+        Me.GMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(105, Byte), Integer), CType(CType(225, Byte), Integer))
+        Me.GMapControl1.ShowTileGridLines = False
+        Me.GMapControl1.Size = New System.Drawing.Size(1196, 688)
         Me.GMapControl1.TabIndex = 0
         Me.GMapControl1.Zoom = 0R
         '
         'UserControl1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.Controls.Add(Me.GMapControl1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "UserControl1"
-        Me.Size = New System.Drawing.Size(800, 450)
+        Me.Size = New System.Drawing.Size(1200, 692)
         Me.ResumeLayout(false)
 
 End Sub
