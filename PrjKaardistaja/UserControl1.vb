6acb8ceaf23bc481682790e47e043f7b29aeee15
Smuutihh <92427508+Smuutihh@users.noreply.github.com>
2023-04-23 16:10:34 +0300

    merge accepted

--- PrjKaardistaja/UserControl1.vb
+++ PrjKaardistaja/UserControl1.vb
@@ -8,5 +8,32 @@
         GMapControl1.Manager.Mode = GMap.NET.AccessMode.ServerAndCache
         GMapControl1.CanDragMap = true
         GMapControl1.DragButton = MouseButtons.Left
+
+
+        Dim busStopOverlay As New GMapOverlay("busStops")
+        GMapControl1.Overlays.Add(busStopOverlay)
+        
+
+
+        Dim andmebaas As PrjAndmebaasKomponent.ISaaAndmed
+        andmebaas = New PrjAndmebaasKomponent.CAndmebaasiKomponent(Application.StartupPath)
+
+        
+
+        GMapControl1.Overlays.Clear()
+
+        Dim markerOverlay As New GMapOverlay("markers")
+        GMapControl1.Overlays.Add(markerOverlay)
+
+
+    ' Add start and end markers
+        Dim startMarker As New GMap.NET.WindowsForms.Markers.GMarkerGoogle(
+        New PointLatLng(andmebaas.saaPeatuseAsukohtLat(startPoint), andmebaas.saaPeatuseAsukohtLong(startPoint)),
+        GMap.NET.WindowsForms.Markers.GMarkerGoogleType.red_dot)
+        Dim endMarker As New GMap.NET.WindowsForms.Markers.GMarkerGoogle(
+        New PointLatLng(andmebaas.saaPeatuseAsukohtLat(endPoint), andmebaas.saaPeatuseAsukohtLong(endPoint)),
+        GMap.NET.WindowsForms.Markers.GMarkerGoogleType.red_dot)
+        markerOverlay.Markers.Add(startMarker)
+        markerOverlay.Markers.Add(endMarker)
     End Sub
 End Class
