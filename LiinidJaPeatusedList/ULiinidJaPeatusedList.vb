6562ad46363889c190538f7eda6d79a026607252
Kris Ilmar Kristenprun <kris.ilmar@gmail.com>
2023-04-23 20:25:48 +0300

    Peatuste nimede saamine liini järgi funktsioon lisatud ja testitud

--- LiinidJaPeatusedList/ULiinidJaPeatusedList.vb
+++ LiinidJaPeatusedList/ULiinidJaPeatusedList.vb
@@ -17,8 +17,28 @@
         Next
     End Sub
     Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListLiinid.SelectedIndexChanged
+        andmebaas = New PrjAndmebaasKomponent.CSaaAndmed(Application.StartupPath)
+
         If ListLiinid.SelectedIndex >= 0 Then
-            MessageBox.Show(ListLiinid.SelectedItem.ToString())
+            Dim koosNimi As String = ListLiinid.SelectedItem.ToString()
+            Dim stringArray As String() = Split(koosNimi)
+            Dim liiniNimi As String = stringArray(0)
+            Dim liiniTeekond As String = stringArray(1)
+            Dim liiniTeekond2 As String = stringArray(2)
+            Dim liiniTeekond3 As String = stringArray(3)
+
+            Dim mingiString As String = liiniNimi & liiniTeekond
+
+
+            Dim uusLiiniTeekond As String = liiniTeekond & " " & liiniTeekond2 & " " & liiniTeekond3
+
+            MsgBox(uusLiiniTeekond & "midagi")
+
+            Dim peatusteNimed As List(Of String) = andmebaas.saaPeatuseNimedLiiniJargi(liiniNimi, uusLiiniTeekond)
+
+            For Each peatus In peatusteNimed
+                MsgBox(peatus)
+            Next
         End If
     End Sub
 
